2891adfe6188ce4428a5a83294d85ff3fdbf6b79
Saurabh Garg <s.garg@alumni.usc.edu>
2017-09-30 16:45:38 -0700

    cleaned up search decorator

--- search.go
+++ search.go
@@ -11,7 +11,12 @@ import (
 	"time"
 )
 
-// Search decorator takes an array of attributes against which search paramater will be matched and it returns enhanced resposne filtered by search criteria
+// Search returns a Decorator which will call the underlying handler and then search for a specified term in the
+// http response. The 'searchable' attributes are passed in the function definition. The search term is read from the
+// 'search' query paramater.
+//
+// Note: While this approach might be used in some cases, in most cases using the search support of app engine
+// would be more efficient.
 func Search(attribute []string) Decorator {
 	return func(h Handler) Handler {
 		return HandlerFunc(func(r *http.Request, ps httprouter.Params, username string) (interface{}, *ServerError) {
@@ -19,40 +24,24 @@ func Search(attribute []string) Decorator {
 			if serverError != nil {
 				return response, serverError
 			}
-			search := getSearchFromQuery(r, username)
-			if search == "" {
+			queryValues := r.URL.Query()
+			searchTerm := queryValues.Get("search")
+			searchTerm = strings.TrimSpace(searchTerm)
+			if searchTerm == "" {
 				return response, nil
 			}
-			limitedResult := limitBySearch(response, attribute, search)
-			return limitedResult.Interface(), nil
+			input := reflect.ValueOf(response)
+			array := reflect.MakeSlice(reflect.TypeOf(response), 0, 10)
+			for i := 0; i < input.Len(); i++ {
+				var res string
+				for j := 0; j < len(attribute); j++ {
+					res = input.Index(i).Elem().FieldByName(attribute[j]).Interface().(string)
+					if strings.Contains(res, searchTerm) == true && searchTerm != "" {
+						array = reflect.Append(array, (input).Index(i))
+					}
+				}
+			}
+			return array.Interface(), nil
 		})
 	}
-}
-
-// limitBySearch limits the results in response by mathcing search params aginst attribute array
-// attributes are basically feild name
-func limitBySearch(response interface{}, attribute []string, search string) reflect.Value {
-	input := reflect.ValueOf(response)
-	// We used reflect package from Go to make pagination Generic
-	//reflect.ValueOf returns a new Value initialized to the concrete value
-	// stored in the interface i.  ValueOf(nil) returns the zero Value.
-	array := reflect.MakeSlice(reflect.TypeOf(response), 0, 10)
-	for i := 0; i < input.Len(); i++ {
-		var res string
-		for j := 0; j < len(attribute); j++ {
-			res = input.Index(i).Elem().FieldByName(attribute[j]).Interface().(string)
-			if strings.Contains(res, search) == true && search != "" {
-				array = reflect.Append(array, (input).Index(i))
-			}
-		}
-	}
-	return array
-}
-
-// getSearchFromQuery to get search term from query params
-func getSearchFromQuery(r *http.Request, username string) string {
-	queryValues := r.URL.Query()
-	search := queryValues.Get("search")
-	search = strings.TrimSpace(search)
-	return search
 }
