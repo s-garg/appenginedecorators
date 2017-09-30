eb64b15b04d43684588f3574183ce4c556b16874
Saurabh Garg <sgarg@twitter.com>
2017-09-29 17:36:48 -0700

    Formatted & cleaned up the handler code

--- handler.go
+++ handler.go
@@ -1,4 +1,3 @@
-// package core contains types and methods for appengine decorators
 package core
 
 import (
@@ -6,27 +5,22 @@ import (
 	"net/http"
 )
 
-// A Handler wraps a Do method that can be registered to a route to handle HTTP
-// requests and has params for additional variables and queries ,at last username is used to lookup user
-// is implemented
+// A Handler represents code that processes a http request and returns response as an interface.
 type Handler interface {
 	Do(r *http.Request, ps httprouter.Params, username string) (interface{}, *ServerError)
 }
 
-// A Handlerfunc wraps a method which implements Handler Interface
+// A HandlerFunc coverts an existing func to type Handler
 type HandlerFunc func(r *http.Request, ps httprouter.Params, username string) (interface{}, *ServerError)
 
 func (f HandlerFunc) Do(r *http.Request, ps httprouter.Params, username string) (interface{}, *ServerError) {
 	return f(r, ps, username)
 }
 
-// A Decorator takes in Handler , applies enhancement to the handler, without modifying its existing properties and return the enhanced handler
+// A Decorator takes in a Handler, applies some enhancement to the existing handler logic and returns the enhanced handler
 type Decorator func(h Handler) Handler
 
-// Decorate is used to bind all the decorators together with the handler,with every loop a decorator is applied, which adds it own enhancement
-// and it is orthogonal to the other decorators
-// Here order of decorators is very important , for e.g. if you want to implement search and pagination, Search decorator should go first and on the result of search decorator
-// i.e  handler resposne with searched criteria , you can apply pagination to limit results.
+// Decorate takes a Handler and a list of Decorators, and converts them to a new Handler which will go through all the Decorators before executing the original Handler.
 func Decorate(h Handler, ds ...Decorator) Handler {
 	decorated := h
 	for _, decorate := range ds {
